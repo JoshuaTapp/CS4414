28c7e5c0e9566281de0a855a89bbdb777fa532b0
JoshuaTapp <jct7bm@virginia.edu>
2022-03-13 01:35:22 -0500

    Pool Finished! few data race errors due to task->Run() and pool-test.cc, waiting to see if pool-test.cc is suppose to produce these

---
+++ pool/pool.cc
@@ -0,0 +1,179 @@
+#include "pool.h"
+
+Task::Task() {
+    // * sema that signals the task is done, shared btw procs    
+    if(sem_init(&this->done, 1, 1) == -1) {
+        printf("Task Semaphore failed to intialize");
+    }    
+    // * Tell ThreadPool, hasnt finished yet
+    sem_wait(&this->done);          
+}
+
+Task::~Task() {
+    sem_destroy(&this->done);
+}
+
+ThreadPool::ThreadPool(int num_threads) {
+
+    // * All the lovely mutexes for each need
+    pthread_mutex_init(&this->threadpool_mutex, NULL);
+    pthread_mutex_lock(&this->threadpool_mutex);
+
+    pthread_mutex_init(&this->queue_mutex, NULL);
+    pthread_mutex_init(&this->map_mutex, NULL);
+
+
+    // * Semaphore to control access to name_map
+    sem_init(&this->map_write, 0, 1);
+
+    // * CVs
+    pthread_cond_init(&this->task_avail, NULL);
+
+    // * pthread_ids for the thread workers
+    this->ptids = new pthread_t[num_threads];
+
+    // * allow ThreadPool's threads to run
+    stop_threads = false;
+    this->num_threads = num_threads;
+
+    // * create threads with their worker
+    int rtn;
+    for(int i = 0; i < num_threads; ++i) {
+        rtn = pthread_create(&this->ptids[i], NULL, &WorkerThread, (void*) this);
+        if(rtn) {
+            printf("Failed pthread_create: %i\n", rtn);
+            exit(-1);
+        }
+    }
+    pthread_mutex_unlock(&this->threadpool_mutex);
+}
+
+
+
+void ThreadPool::SubmitTask(const std::string &name, Task* task) {
+    // * add task to queue safely
+    pthread_mutex_lock(&this->queue_mutex);
+    this->task_queue.push_back(task);
+    pthread_mutex_unlock(&this->queue_mutex);
+    
+    // * signal to threads that task is available
+    pthread_cond_signal(&this->task_avail); 
+
+    // * add {name, task} to map safely
+    sem_wait(&this->map_write);
+    this->name_map.emplace(name, task);
+    sem_post(&this->map_write);
+}
+
+/*
+    * Returns Task pointer from queue safely
+    * When Stop() is called, returns nullptr
+*/
+
+Task* ThreadPool::GetTask() {
+    pthread_mutex_lock(&this->queue_mutex);
+    
+    Task* task = nullptr;
+
+    // * Wait for work if queue is empty
+    while(task_queue.empty()) {
+        pthread_cond_wait(&this->task_avail, &this->queue_mutex);
+    }
+
+    // * dequeue a task from task_queue
+    task = this->task_queue.front();
+    this->task_queue.pop_front();
+    
+    pthread_mutex_unlock(&this->queue_mutex);
+
+    return task;
+}
+
+/*
+    * Uses the Task's semaphore to wait until the task is done.
+    * The if cond only execute if the Task has not been started
+    * or the task is currently running when called.
+*/
+void ThreadPool::WaitForTask(const std::string &name) {
+    Task* task = this->GetTaskFromMap(name);
+    // * if this fails, the task is already done.
+    if(task != nullptr) {
+        // * Will only continue when task is done
+        sem_wait(&task->done);
+        sem_post(&task->done);
+    }
+
+    // * Now remove this entry from the name_map
+    pthread_mutex_lock(&this->map_mutex);
+    this->name_map.erase(name);
+    pthread_mutex_unlock(&this->map_mutex);
+}
+
+Task* ThreadPool::GetTaskFromMap(const std::string &name) {
+
+    sem_wait(&this->map_write);
+    auto it = this->name_map.find(name);
+    sem_post(&this->map_write);
+    if(it != this->name_map.end()) {
+        return it->second;
+    }
+    else return nullptr;
+}
+
+
+void ThreadPool::Stop() {
+    
+    pthread_mutex_lock(&this->threadpool_mutex);
+    // * Tell threads to finish their current task
+    this->stop_threads = true;
+    pthread_mutex_unlock(&this->threadpool_mutex);
+
+    // * Place null items on stack to get threads unstuck in GetTask()
+    for(int i = 0; i < this->num_threads; ++i) {
+        this->SubmitTask("", nullptr);
+    }
+    
+    // * now that all threads are finished, join them
+    for(int i = 0; i < this->num_threads; ++i) {
+        if(pthread_join(this->ptids[i], NULL)) {
+            printf("pthread_join() failed\n");
+        }
+    }
+    
+    // * Delete the things currently on Heap
+    delete [] this->ptids;
+    pthread_mutex_destroy(&this->threadpool_mutex);
+    pthread_mutex_destroy(&this->queue_mutex);
+    pthread_mutex_destroy(&this->map_mutex);
+    pthread_cond_destroy(&this->task_avail);
+    sem_destroy(&this->map_write);
+}
+
+/*
+    * This is the function the threads within the thread pool
+    * will execute. The function runs 
+*/
+void* ThreadPool::WorkerThread(void* args) {
+    ThreadPool* pool = (ThreadPool*) args;
+
+    while(true) { // ? What should be in here? a variable, or handle stop elsewhere?
+        // * get task using thread safe method
+        Task* task = pool->GetTask();   
+
+        // * run task using the Task's implementation
+        // * if we have a nullptr, then Stop() has been called.
+        if(task != nullptr) {
+            task->Run();                    
+        }
+        else break;
+        
+        // * task->done = 1 now, WaitForTask() can proceed
+        sem_post(&task->done);          
+        delete task;
+              
+        pthread_mutex_lock(&pool->threadpool_mutex);
+        if(pool->stop_threads) break;
+        pthread_mutex_unlock(&pool->threadpool_mutex);
+    }
+    return NULL;
+}
---
+++ pool/pool.h
@@ -0,0 +1,63 @@
+#ifndef POOL_H_
+#include <string>
+#include <pthread.h>
+#include <deque>
+#include <map>
+#include <semaphore.h>
+
+
+class Task {
+public:
+    sem_t done;     // * Used by WaitForTask to signal task is complete
+    Task();
+    virtual ~Task();
+
+    virtual void Run() = 0;  // implemented by subclass
+};
+
+class ThreadPool {
+public:
+    // * array that holds all the pthread ids
+    pthread_t *ptids;
+
+    // * Mutexes for ThreadPool, Queue, and Map
+    pthread_mutex_t threadpool_mutex;
+    pthread_mutex_t queue_mutex;
+    pthread_mutex_t map_mutex;
+
+    // * CV for GetTask/SubmitTask when queue is empty
+    pthread_cond_t task_avail;
+
+    // * map_write acts like a lock
+    sem_t map_write;
+
+    // * stop_threads controls WorkerThreads
+    bool stop_threads;
+    int num_threads;
+
+    std::deque<Task*> task_queue;
+    std::map<std::string, Task*> name_map;
+
+
+    ThreadPool(int num_threads);
+
+    // Submit a task with a particular name.
+    void SubmitTask(const std::string &name, Task *task);
+ 
+    // Wait for a task by name, if it hasn't been waited for yet. Only returns after the task is completed.
+    void WaitForTask(const std::string &name);
+
+    // Stop all threads. All tasks must have been waited for before calling this.
+    // You may assume that SubmitTask() is not caled after this is called.
+    void Stop();
+
+    // * Used by WaitForTask to convert name -> Task* safely
+    Task* GetTaskFromMap(const std::string &name);
+
+    // * Consumer of Tasks from task_queue    
+    Task* GetTask();
+
+    // * Function pointer that runs Tasks
+    static void* WorkerThread(void* args);
+};
+#endif
